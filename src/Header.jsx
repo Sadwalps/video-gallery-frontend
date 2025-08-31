563d7d3998de6e939cca650eb131c696a76254c2
Sadwalps <sadwalps2255@gmail.com>
2025-08-31 22:26:17 +0530

    navbar almost completed

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,33 @@
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import videoGalleryIcon from './assets/video-gallery-icon.png'
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <>
-    
+      <Navbar className="">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              alt=""
+              src={videoGalleryIcon}
+              width="50"
+              height="45"
+              className="d-inline-block align-top"
+            />{' '}
+
+          </Navbar.Brand>
+          <div className='hearticondiv d-flex flex-column justify-content-center align-items-center ' >
+           <Link to={'likedvideos'} className='hearticon'><FontAwesomeIcon icon={faHeart}  /></Link> 
+           
+          </div>
+          
+        </Container>
+      </Navbar>
     </>
   )
 }

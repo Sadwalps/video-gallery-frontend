efbd124d6fca2cf4eba5a7e3b30c7bfe520a6dd2
Sadwalps <sadwalps2255@gmail.com>
2025-09-14 15:23:23 +0530

    page not found

--- src/Pagenotfound.jsx
+++ src/Pagenotfound.jsx
@@ -9,8 +9,8 @@ function Pagenotfound() {
       <div className="col-md-1"></div>
       <div className="col-md-10 p-2 d-flex flex-column justify-content-center align-items-center " style={{ height:"100vh"}}>
         
-        <h1 className=''>404</h1>
-        <h2>Page Not Found</h2>
+        <h1 className='pnfheading'>404</h1>
+        <h2 className='pnfsubheading'>Page Not Found</h2>
          <Link to={"/"}> <button className='btn px-3 py-2 border border-3 border-danger  w-100 py-lg-2' id='removebtn' > back to home</button></Link>
           
           
